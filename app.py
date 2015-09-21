e3e29f97449b807f5d7df4402495bc9e3122e131
russ <russell.sayers@gmail.com>
2015-09-21 10:48:14 -0700

    readme info

--- app.py
+++ app.py
@@ -20,12 +20,16 @@ def index():
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    emit('my response', {'data': 5})
+    emit('my response', {'data': 5, 'r':255, 'g':255, 'b':0})
+    emit('my response', {'data': 6, 'r':255, 'g':255, 'b':0})
     time.sleep(1)
-    emit('my response', {'data': 6})
+    emit('my response', {'data': 5, 'r':0, 'g':0, 'b':0})
+    emit('my response', {'data': 6, 'r':255, 'g':255, 'b':0})
+    emit('my response', {'data': 7, 'r':255, 'g':255, 'b':0})
     time.sleep(1)
-    emit('my response', {'data': 7})
-    emit('my response', {'data': 8})
+    emit('my response', {'data': 6, 'r':0, 'g':0, 'b':0})
+    emit('my response', {'data': 7, 'r':255, 'g':255, 'b':0})
+    emit('my response', {'data': 8, 'r':255, 'g':255, 'b':0})
 
 
 @socketio.on('disconnect', namespace='/test')
---
+++ readme.md
@@ -0,0 +1,5 @@
+Handy Links
+
+* [Easy WebSockets with Flask and Gevent - miguelgrinberg.com ](http://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent)
+* [miguelgrinberg/Flask-SocketIO](https://github.com/miguelgrinberg/Flask-SocketIO)
+* [Docker Explained: How To Containerize Python Web Applications | DigitalOcean](https://www.digitalocean.com/community/tutorials/docker-explained-how-to-containerize-python-web-applications)
