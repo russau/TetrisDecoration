50e9b37da1c8355979011f8ccbee18af745f40ae
russ <russell.sayers@gmail.com>
2015-09-27 22:39:09 -0700

    background thread testing

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+#http://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
+#http://www.alexhadik.com/blog/2015/1/29/using-socketio-with-python-and-flask-on-heroku
+# apt-get install python-dev
+# apt-get install -y gcc
+# pip install  gevent-socketio
+# pip install gevent-websocket
+# pip install gevent
+
 from gevent import monkey
-monkey.patch_all()
+# monkey.patch_all()
 
 import time
 from threading import Thread
@@ -10,27 +18,34 @@ from flask.ext.socketio import SocketIO, emit, join_room, leave_room, \
 app = Flask(__name__)
 app.debug = True
 socketio = SocketIO(app)
+thread = None
 
-
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    print "XXXX"
+    while True:
+        time.sleep(5)
+        squares = [{'r':255, 'g':255, 'b':0} for x in range(200)]
+        socketio.emit('my response', squares, namespace='/test')
+        time.sleep(5)
+        squares = [{'r':128, 'g':0, 'b':128} for x in range(200)]
+        socketio.emit('my response', squares, namespace='/test')
+        time.sleep(5)
+        squares = [{'r':0, 'g':0, 'b':255} for x in range(200)]
+        socketio.emit('my response', squares, namespace='/test')
 
 @app.route('/')
 def index():
+    global thread
+    print "starting thread"
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.start()
     return render_template('index.html')
-
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    emit('my response', {'data': 5, 'r':255, 'g':255, 'b':0})
-    emit('my response', {'data': 6, 'r':255, 'g':255, 'b':0})
-    time.sleep(1)
-    emit('my response', {'data': 5, 'r':0, 'g':0, 'b':0})
-    emit('my response', {'data': 6, 'r':255, 'g':255, 'b':0})
-    emit('my response', {'data': 7, 'r':255, 'g':255, 'b':0})
-    time.sleep(1)
-    emit('my response', {'data': 6, 'r':0, 'g':0, 'b':0})
-    emit('my response', {'data': 7, 'r':255, 'g':255, 'b':0})
-    emit('my response', {'data': 8, 'r':255, 'g':255, 'b':0})
-
+    print('Client connected')
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
@@ -38,4 +53,4 @@ def test_disconnect():
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0')
