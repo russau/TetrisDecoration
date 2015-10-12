4b81017cd385b6c41425735c59bb60e9f32e158e
russ <russell.sayers@gmail.com>
2015-10-11 22:05:33 -0700

    moved all tetris code into Tetris.py, and now using a function pointer for display

--- Tetris.py
+++ Tetris.py
@@ -1,4 +1,5 @@
 import time
+import random
 from copy import copy
 
 BOARD_HEIGHT = 8
@@ -9,6 +10,115 @@ def unshared_copy(inList):
         return list( map(unshared_copy, inList) )
     return inList
 
+# tetrisbuster code
+
+pieces = {
+		'i':[ [ [True, False, False, False],[True, False, False, False],[True, False, False, False],[True, False, False, False] ], [  [True, True, True, True],[False, False, False, False],[False, False, False, False],[False, False, False, False] ], [ [True, False, False, False],[True, False, False, False],[True, False, False, False],[True, False, False, False] ], [ [True, True, True, True],[False, False, False, False],[False, False, False, False],[False, False, False, False] ] ],
+		'j':[ [ [False, True, False, False],[False, True, False, False],[True, True, False, False],[False, False, False, False] ], [ [True, False, False, False],[True, True, True, False],[False, False, False, False],[False, False, False, False] ], [ [True, True, False, False],[True, False, False, False],[True, False, False, False],[False, False, False, False] ], [ [True, True, True, False],[False, False, True, False],[False, False, False, False],[False, False, False, False] ] ],
+		'l':[ [ [True, False, False, False],[True, False, False, False],[True, True, False, False],[False, False, False, False] ], [ [True, True, True, False],[True, False, False, False],[False, False, False, False],[False, False, False, False] ], [ [True, True, False, False],[False, True, False, False],[False, True, False, False],[False, False, False, False] ], [ [False, False, True, False],[True, True, True, False],[False, False, False, False],[False, False, False, False] ] ],
+		'o':[ [ [True, True, False, False],[True, True, False, False],[False, False, False, False],[False, False, False, False] ], [  [True, True, False, False],[True, True, False, False],[False, False, False, False],[False, False, False, False] ], [  [True, True, False, False],[True, True, False, False],[False, False, False, False],[False, False, False, False] ], [  [True, True, False, False],[True, True, False, False],[False, False, False, False],[False, False, False, False] ] ],
+		's':[ [ [False, True, True, False],[True, True, False, False],[False, False, False, False],[False, False, False, False] ], [  [True, False, False, False],[True, True, False, False],[False, True, False, False],[False, False, False, False] ], [ [False, True, True, False],[True, True, False, False],[False, False, False, False],[False, False, False, False] ], [ [True, False, False, False],[True, True, False, False],[False, True, False, False],[False, False, False, False] ] ],
+		't':[ [ [True, True, True, False],[False, True, False, False],[False, False, False, False],[False, False, False, False] ], [ [False, True, False, False],[True, True, False, False],[False, True, False, False],[False, False, False, False] ], [ [False, True, False, False],[True, True, True, False],[False, False, False, False],[False, False, False, False] ], [ [True, False, False, False],[True, True, False, False],[True, False, False, False],[False, False, False, False] ], ],
+		'z':[ [ [True, True, False, False],[False, True, True, False],[False, False, False, False],[False, False, False, False] ], [ [False, True, False, False],[True, True, False, False],[True, False, False, False],[False, False, False, False] ], [ [True, True, False, False],[False, True, True, False],[False, False, False, False],[False, False, False, False] ], [ [False, True, False, False],[True, True, False, False],[True, False, False, False],[False, False, False, False] ], ],
+	}
+
+board_height = 8
+board_width = 8
+
+def getPositionAndDegrees(board, piece):
+	board = board.split()
+
+	outstr = ""
+
+	# try every combination and compute a score for that combo.
+	combos = {}
+	for x in range(board_width):
+		for rot in range(4):
+			new_board, legal = doMove(board, piece, x, rot)
+			if legal:
+				# determine how many points this board is worth
+				pts = 0
+				for by in range(board_height):
+					# negative score for each block, blocks toward the bottom
+					# don't hurt as much
+					pts += sum(-10*(board_height-by) for char in new_board[by] if char != ".")
+					# find holes and give them a penalty
+					for bx in range(board_width):
+						if new_board[by][bx] == '.':
+							# see if there is a block anywhere above it
+							for check_y in range(by):
+								if new_board[check_y][bx] != ".":
+									pts -= 50 * (board_height-check_y)
+									break
+
+				combos[pts] = [x, rot]
+
+	# best combo is lowest key
+	keys = combos.keys()
+	keys.sort(reverse=True)
+
+	# log
+	if len(keys) > 0:
+		new_board, legal = doMove(board, piece, combos[keys[0]][0],  combos[keys[0]][1])
+		outstr += "\nexpected board:\n%s" % '\n'.join(new_board)
+#	open('lastrun.txt', 'a').write("\n---------------------\nboard:\n%s\npiece: %s\n combos: %s\noutstr: %s\n" % ('\n'.join(board), piece, combos, outstr))
+
+	if len(keys) == 0:
+		# random, we're about to lose
+		position = 0
+		degrees = 0
+	else:
+		position = combos[keys[0]][0]
+		degrees = combos[keys[0]][1] * 90
+	return position, degrees
+
+def canPutPiece(board, piece, left, top, rot):
+	new_board = board[:]
+
+	for y in range(4):
+		for x in range(4):
+			bx = left+x
+			by = top+y
+
+
+			if pieces[piece][rot][y][x]:
+				if by >= board_height or bx >= board_width or \
+				bx < 0 or by < 0 or board[by][bx] != '.':
+					return (board, False,) # illegal move
+				else:
+					row = list(new_board[by])
+					row[bx] = piece
+					new_board[by] = ''.join(row)
+
+	return (new_board, True,) # legal move
+
+def doMove(board, piece, position, rot):
+	# put piece at top of board
+	y = 0
+
+	# move piece down until we can't anymore
+	new_board = board[:]
+	while True:
+		ret_board = new_board[:]
+		new_board, legal = canPutPiece(board, piece, position, y, rot)
+		if not legal:
+			break
+
+		y += 1
+
+	# delete complete rows
+	ret_board = filter(lambda row: any(item == '.' for item in row), ret_board)
+	for i in range(board_height-len(ret_board)):
+		ret_board.insert(0, '.' * board_width)
+
+
+	if y == 0:
+		# cannot place piece; return illegal move
+		return (board, False,)
+	else:
+		# put the changes into board
+		return (ret_board, True,) # 'True' - move is legal
+
 
 class Tetris:
 
@@ -16,6 +126,11 @@ class Tetris:
         self.board = [['.' for _ in range(BOARD_WIDTH)] for x in range(BOARD_HEIGHT)]
         self.gameResults = []
         self.emitter = emitter
+
+    def dropRandom(self):
+        piece = random.choice(['i','j','l','o','s','t','z'])
+        position, degrees = getPositionAndDegrees(self.boardToPOSTString(), piece)
+        return self.dropPiece(position, piece, degrees)
 
     def dropPiece(self, pos, piecech, degrees):
         if piecech == 'i':
@@ -69,7 +184,8 @@ class Tetris:
                     if rotated[y][x]==1:
                         frame[n-y][destx+x] = piecech
             squares = self.boardToLights(frame)
-            self.emitter.emit('my response', squares, namespace='/test')
+            #self.emitter.emit('my response', squares, namespace='/test')
+            self.emitter(squares)
             time.sleep(0.5)
 
         # copy the rotated piece to its dest on the board
@@ -78,7 +194,8 @@ class Tetris:
                 if rotated[y][x]==1:
                     self.board[desty-y][destx+x] = piecech
         squares = self.boardToLights(self.board)
-        self.emitter.emit('my response', squares, namespace='/test')
+        #self.emitter.emit('my response', squares, namespace='/test')
+        self.emitter(squares)
         time.sleep(0.5)
 
         # test for rows to remove - could be a bit more efficient: break out when u hit a blank row
@@ -93,11 +210,13 @@ class Tetris:
             if allfull:
                 self.board[row] = ['f1' for _ in range(BOARD_WIDTH)]
                 squares = self.boardToLights(self.board)
-                self.emitter.emit('my response', squares, namespace='/test')
+                #self.emitter.emit('my response', squares, namespace='/test')
+                self.emitter(squares)
                 time.sleep(0.25)
                 self.board[row] = ['f2' for _ in range(BOARD_WIDTH)]
                 squares = self.boardToLights(self.board)
-                self.emitter.emit('my response', squares, namespace='/test')
+                #self.emitter.emit('my response', squares, namespace='/test')
+                self.emitter(squares)
                 time.sleep(0.25)
 
                 del self.board[row]
@@ -107,8 +226,8 @@ class Tetris:
                 row -= 1
 
         squares = self.boardToLights(self.board)
-
-        self.emitter.emit('my response', squares, namespace='/test')
+        #self.emitter.emit('my response', squares, namespace='/test')
+        self.emitter(squares)
 
         return True
 
@@ -169,3 +288,15 @@ class Tetris:
                 print self.board[y][x],
             print ""
         print " ".join("=" for _ in range(BOARD_WIDTH))
+
+def squaresEmitter(squares):
+    print "hi"
+
+if __name__ == '__main__':
+    while True:
+        print "New Tetris class"
+        t = Tetris(squaresEmitter)
+        result = True
+
+        while result:
+            result = t.dropRandom()
