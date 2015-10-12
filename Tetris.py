8b50583745110fd75a6306169ee267c2cb0be42c
russ <russell.sayers@gmail.com>
2015-10-12 05:33:47 +0000

    Neopixels code 😎

--- Tetris.py
+++ Tetris.py
@@ -1,6 +1,7 @@
 import time
 import random
 from copy import copy
+from neopixel import *
 
 BOARD_HEIGHT = 8
 BOARD_WIDTH = 8
@@ -290,9 +291,23 @@ class Tetris:
         print " ".join("=" for _ in range(BOARD_WIDTH))
 
 def squaresEmitter(squares):
-    print "hi"
+    for i in range(strip.numPixels()):
+        color = squares[i]
+        strip.setPixelColorRGB(i, color['r'], color['g'], color['b']);
+    strip.show()
 
 if __name__ == '__main__':
+    # LED strip configuration:
+    LED_COUNT      = 64      # Number of LED pixels.
+    LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+    LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+    LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+    LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+    LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    strip.begin()
+
     while True:
         print "New Tetris class"
         t = Tetris(squaresEmitter)
@@ -300,3 +315,4 @@ if __name__ == '__main__':
 
         while result:
             result = t.dropRandom()
+
